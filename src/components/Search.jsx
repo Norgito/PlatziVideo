e7afd87e2135b3ba1869253014dc38d3416911ad
Norgito <tito.cubanito@gmail.com>
2022-12-28 17:38:03 -0300

    counter redux

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import '../assets/styles/components/Search.scss';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "../store/slices/counter/counterSlice";
+import "../assets/styles/components/Search.scss";
 
-const Search = () => (
-  <section className="main">
-    <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-    <input type="text" className="input_search" placeholder="Buscar..." />
-  </section>
-);
+
+
+
+const Search = () => {
+  const { counter } = useSelector(state => state.counter)
+  const dispatch = useDispatch();
+  return (
+    <section className="main">
+      <h2 className="main__title">{counter}</h2>
+      <div className="espacio">
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByAmount(2))}>Increment + 2</button>
+      </div>
+      <input type="text" className="input_search" placeholder="Buscar..." />
+    </section>
+  )
+}
 
 export default Search;
