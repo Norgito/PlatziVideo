2b01335984757344de547dc92b65b3b6823e7ca0
Norgito <tito.cubanito@gmail.com>
2022-12-29 23:53:29 -0300

    back to original

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from "../store/slices/counter/counterSlice";
+
 import "../assets/styles/components/Search.scss";
 
 const Search = () => {
-  const { counter } = useSelector((state) => state.counter);
-  const dispatch = useDispatch();
+
   return (
     <section className="main">
-      <h2 className="main__title">{counter}</h2>
+      <h2 className='main__title'>¿Qué quieres ver hoy?</h2>
       <div className="espacio">
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-        <button onClick={() => dispatch(incrementByAmount(2))}>
-          Increment + 2
-        </button>
       </div>
       <input type="text" className="input_search" placeholder="Buscar..." />
     </section>
@@ -26,3 +15,6 @@ const Search = () => {
 };
 
 export default Search;
+
+
+
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import Search from "../components/Search";
 import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
+
 import "../assets/styles/App.scss";
 
-const Home = () => {
-  const { myList, trends, originals } = useSelector(state => state.item)
+const Home = ({ mylist, trends, originals }) => {
   return (
     <>
       <Search />
-      {myList.length > 0 && (
+
+      {mylist.length > 0 && (
         <Categories title="Mi Lista">
           <Carousel>
-            {{ item }.myList.map((item) => (
-              <CarouselItem key={item.id} {...item} />
+            {mylist.map((item) => (
+              <CarouselItem key={item.id} {...item} isList />
             ))}
           </Carousel>
         </Categories>
       )}
+
       <Categories title="Tendencias">
         <Carousel>
           {trends.map((item) => (
@@ -27,6 +29,7 @@ const Home = () => {
           ))}
         </Carousel>
       </Categories>
+
       <Categories title="Originales de Platzi Video">
         <Carousel>
           {originals.map((item) => (
@@ -38,4 +41,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    mylist: state.mylist,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
